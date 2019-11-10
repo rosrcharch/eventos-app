edcecc068bcd0fd8b3cbd07c59484f47ab10e388
rosrcharch <duque_rogerio@hotmail.com>
2019-11-10 00:58:40 -0300

    terminando a primeira tela

--- app/src/main/java/com/example/festaseeventos/MainActivity.java
+++ app/src/main/java/com/example/festaseeventos/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.festaseeventos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnCadastrarFesta;
+    private Button btnCadastrarEmpresa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCadastrarFesta = findViewById(R.id.btnCadastrarFesta);
+        btnCadastrarEmpresa = findViewById(R.id.btnOferecerServico);
+    }
+
+
+    public void cadastrarFesta(View view){
+
+        startActivity(new Intent(this, TelaCadastrarFesta.class));
+
+    }
+
+    public void cadastrarEmpresa(View view){
+
+        startActivity(new Intent(this, TelaOferecerServico.class));
+
+
     }
 }
---
+++ app/src/main/java/com/example/festaseeventos/TelaCadastrarFesta.java
@@ -0,0 +1,16 @@
+package com.example.festaseeventos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaCadastrarFesta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_festa);
+
+
+    }
+}
