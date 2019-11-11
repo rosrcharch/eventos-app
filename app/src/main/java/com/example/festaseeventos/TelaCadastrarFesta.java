9feefd23983f98e602033d5bff777d756f679f74
rosrcharch <duque_rogerio@hotmail.com>
2019-11-11 17:22:08 -0300

    criação da tela de cadastro das festas, opçoes de layout

--- app/src/main/java/com/example/festaseeventos/TelaCadastrarFesta.java
+++ app/src/main/java/com/example/festaseeventos/TelaCadastrarFesta.java
@@ -1,16 +1,29 @@
 package com.example.festaseeventos;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class TelaCadastrarFesta extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_festa);
 
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
