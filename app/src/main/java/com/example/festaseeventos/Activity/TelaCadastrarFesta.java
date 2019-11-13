7b648c067953fe3b5b5eb81728c0b1c6360fb4b9
rosrcharch <duque_rogerio@hotmail.com>
2019-11-13 11:23:39 -0300

    salvando os dados para recuperação

--- app/src/main/java/com/example/festaseeventos/Activity/TelaCadastrarFesta.java
+++ app/src/main/java/com/example/festaseeventos/Activity/TelaCadastrarFesta.java
@@ -1,11 +1,7 @@
 package com.example.festaseeventos.Activity;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.fragment.app.ListFragment;
 import androidx.viewpager.widget.ViewPager;
 
